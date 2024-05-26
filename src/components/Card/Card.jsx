b3563261088cf3bb181db1dc2b92c5a303849008
Yvens Almeida <yvensalmeida21@gmail.com>
2024-05-26 16:50:06 -0300

    Commit para mudanças de componentes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import './Card.css';
+import './Card.css'
 
 function Card(){
     return(
-        <div className="card"></div>
+        <div className="card">
+            <span>CEP:</span>
+            <span>Cidade:</span>
+            <span>Teste:</span>
+        </div>
     );
-};
+;}
 
 export default Card;
